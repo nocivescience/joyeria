4561b589382426b0e7e2406f116b6fff3fc9d357
ricardo <nocive.science@gmail.com>
2023-07-28 03:32:00 -0400

    primer commit

---
+++ database.sql
@@ -0,0 +1,22 @@
+-- Active: 1690526732239@@127.0.0.1@5432@postgres
+-- CREATE TABLE usuarios(
+--     id SERIAL,
+--     nombre VARCHAR(50),
+--     apellido VARCHAR(50)
+-- )
+
+-- insert into usuarios(nombre, apellido) values('Juan', 'Perez');
+
+-- select * from usuarios;
+
+-- insert into usuarios (id, nombre, apellido)
+-- values  (2, 'Romeo', 'Santelices'),
+--         (3, 'Paolo', 'Camardo'),
+--         (4, 'Sandra', 'Castillo'),
+--         (5, 'Carlos', 'Belmar');
+
+-- select * from usuarios;
+
+-- delete from usuarios where id = 4;
+
+select * from usuarios;
