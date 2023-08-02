7842e6907a4490d5deb1a232121f9d65e8682507
ricardo <nocive.science@gmail.com>
2023-08-02 18:53:11 -0400

    algo pasa con el mime aun no resuelto

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,47 +1,91 @@
+import React, {useState, useEffect} from "react";
+import {useNavigate, useParams} from "react-router-dom";
 import "../files/form.css";
 export const Form = () => {
+  const [usuarios, setUsuarios] = useState({
+    name: "",
+    surname: "",
+  });
+  const [loading, setLoading]= useState(false);
+  const [editing, setEditing]= useState(false);
+  const navigate = useNavigate();
+  const params = useParams();
+  useEffect(() => {
+    if(params.id){
+      loadTask(params.id);
+    }
+  }, [params.id]);
+  const loadTask = async (id) => {
+    const res = await fetch(`http://localhost:4000/users/${id}`);
+    const data = await res.json();
+    setUsuarios({
+      name: data.name,
+      surname: data.surname,
+    });
+    setEditing(true);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log('clickeo');
+    if(params.id){
+      const response = await fetch(`http://localhost:4000/users/${params.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(usuarios),
+      });
+      await response.json();
+      console.log(response);
+    }else{
+      const response = await fetch("http://localhost:4000/users", {
+        method: "POST",
+        body: JSON.stringify(usuarios),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      await response.json();
+      console.log(response);
+    }
+    setLoading(false);
+    navigate("/");
+  };
+  const handleChange = (e) => {
+    setUsuarios({
+      ...usuarios,
+      [e.target.name]: e.target.value,
+    })
+  };
   return (
-    <form className="form-jewel">
-      <h1 className="titulo">Su Pedido</h1>
-      <div className="container">
+    <form className="form-jewel w-50" onSubmit={handleSubmit}>
+      <div className="mob-3">
         <div className="form-group">
-          <label className="nombre" htmlFor="name">Nombre</label>
-          <input className="entrada" id="name" type="text"></input>
+          <h1 className="display-1 d-flex justify-content-center"
+            style={{ 
+              textShadow: "2px 2px 10px white",
+             }}
+          >Formulario</h1>
         </div>
-        <div className="form-group">
-          <label className="nombre" htmlFor="lastname">Apellido</label>
-          <input className="entrada" id="lastname" type="text"></input>
+        <div className="form-group"
+          style={{ marginTop: "1rem" }}
+        >
+          <label htmlFor="name" className="form-label">Nombre</label>
+          <input type="text" id="name" className="form-control" value={usuarios.name} name="name" onChange={handleChange} />
         </div>
-        <div className="form-group">
-          <label className="nombre" htmlFor="email">Correo</label>
-          <input className="entrada" id="email" type="email"></input>
+        <div className="form-group"
+          style={{ marginTop: "1rem" }}
+        >
+          <label htmlFor="surname" className="form-label">Apellido</label>
+          <input type="text" id="surname" className="form-control" value={usuarios.surname} name='surname' onChange={handleChange} />
         </div>
-        <div className="form-group">
-          <label className="nombre" htmlFor="number">Celular</label>
-          <input className="entrada" id="number" type="text"></input>
-        </div>
-        <div className="form-group">
-          <label className="nombre" htmlFor="piedra">Tipo de Gema</label>
-          <select className="piedra seleccion" id='piedra'>
-            <option value='ruby'>Ruby</option>
-            <option value='diamante'>Diamante</option>
-            <option value='sirconio'>Sircolnio</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label className="nombre" htmlFor="metal">Tipo de Metal</label>
-          <select className="metal seleccion" id='metal'>
-            <option value='oro-rosado'>Oro Rosado</option>
-            <option value='oro-blanco'>Oro Blanco</option>
-            <option value='oro-amarillo'>Oro Amarillo</option>
-            <option value='platino'>Platino</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <button className="boton">Enviar Formulario</button>
-        </div>
-        <div className="form-group">
-          <button className="boton clear">Borrar Formulario</button>
+        <div className="form-group"
+          style={{ marginTop: "50px" }}
+        >
+          <button type="button" className="btn btn-dark">
+            Enviar
+          </button>
         </div>
       </div>
     </form>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -29,172 +29,172 @@ import rubia from "../images/rubia.jpg";
 import { NavLink } from "react-router-dom";
 export function Gallery() {
   return (
-      <div class="row position-absolute" style={{top:'10vh',opacity:.8}}>
-        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+      <div className="row position-absolute" style={{top:'10vh',opacity:.8}}>
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
           <NavLink to='/mirando'>
           <img
             src={mirando}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
           </NavLink>
           <NavLink to='/taza'>
           <img
             src={taza}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Wintry Mountain Landscape"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/anillo'>
           <img
             src={anillo}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Mountains in the Clouds"
           />
           </NavLink>
           <NavLink to='/blacklady'>
           <img
             src={blacklady}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
           <NavLink to='/whitehat'>
           <img
             src={whitehat}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
           </NavLink>
           <NavLink to='/jacket'>
           <img
             src={jacket}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Wintry Mountain Landscape"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/laughing'>
           <img
             src={laughing}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Mountains in the Clouds"
           />
           </NavLink>
           <NavLink to='/morena'>
           <img
             src={morena}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/chain'>
           <img
             src={chain}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Waves at Sea"
           />
           </NavLink>
           <NavLink to='/fondo'>
           <img
             src={fondo}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Yosemite National Park"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/mujertriste'>
           <img
             src={mujertriste}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Waves at Sea"
           />
           </NavLink>
           <NavLink to='/sombrerorojo'>
           <img
             src={sombrerorojo}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Yosemite National Park"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
           <NavLink to='/womandreaming'>
           <img
             src={womandreaming}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
           </NavLink>
           <NavLink to='/matrimonio'>
           <img
             src={matrimonio}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Wintry Mountain Landscape"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/blackblondie'>
           <img
             src={blackblondie}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Mountains in the Clouds"
           />
           </NavLink>
           <NavLink to='/luxury'>
           <img
             src={luxury}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/mask'>
           <img
             src={mask}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Waves at Sea"
           />
           </NavLink>
           <NavLink to='/native'>
           <img
             src={native}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Yosemite National Park"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/fotografa'>
           <img
             src={fotografa}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Waves at Sea"
           />
           </NavLink>
           <NavLink to='/pearsing'>
           <img
             src={pearsing}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Yosemite National Park"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
           <NavLink to='/collar_lujoso'>
           <img
             src={collar_lujoso}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
           </NavLink>
@@ -202,55 +202,55 @@ export function Gallery() {
           <NavLink to='/anillo_lujoso'>
           <img
             src={anillo_lujoso}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Wintry Mountain Landscape"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/purpura'>
           <img
             src={purpura}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Mountains in the Clouds"
           />
           </NavLink>
           <NavLink to='/fondo3'>
           <img
             src={fondo3}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Boat on Calm Water"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/anillo_antiguo'>
           <img
             src={anillo_antiguo}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Waves at Sea"
           />
           </NavLink>
           <NavLink to='/boda'>
           <img
             src={boda}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Yosemite National Park"
           />
           </NavLink>
         </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div className="col-lg-4 mb-4 mb-lg-0">
           <NavLink to='/rubia'>
           <img
             src={rubia}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Waves at Sea"
           />
           </NavLink>
           <NavLink to='/manos'>
           <img
             src={manos}
-            class="w-100 shadow-1-strong rounded mb-4"
+            className="w-100 shadow-1-strong rounded mb-4"
             alt="Yosemite National Park"
           />
           </NavLink>
